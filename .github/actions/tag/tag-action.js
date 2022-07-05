f378241a5222b75c7baebfc6482672e8e6b08e39
wai-calvin <calvinw@liatrio.com>
2022-07-05 09:53:48 -0700

    checking logic for incrementing rc number

--- .github/actions/tag/tag-action.js
+++ .github/actions/tag/tag-action.js
@@ -3,22 +3,27 @@ const github = require('@actions/github');
 
 async function action() {
     
+    //Grab inputs from action for auth token and whether or not this is a release candidate
     const myToken = core.getInput('token');
     const octokit = github.getOctokit(myToken);
     const releaseCandidate = core.getInput('release-candidate');
 
+    //Grab all git tags for a specified repo
     const tags = await octokit.rest.repos.listTags({
         owner: "wai-calvin",
         repo: github.context.payload.repository.name
     });
 
+    //Variable for storing the most recent git tag
     const recentGitTag = tags.data[0].name;
 
     if(releaseCandidate == "true") {
         if(!recentGitTag.includes("-rc")) {
+            //Grab patch number and increment by 1
             const oldPatchNum = recentGitTag.split('.').pop();
             const newPatchNum = Number(oldPatchNum)+1;
     
+            //Add '-rc' to git tag
             const createTag = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
                 owner: "wai-calvin",
                 repo: github.context.payload.repository.name,
@@ -29,6 +34,7 @@ async function action() {
         }
     
         else {
+            //Grab 
             const oldRCNum = recentGitTag.slice(-1);
             const newRCNum = Number(oldRCNum)+1;
             const gitTagVersion = recentGitTag.split('-', 1);
