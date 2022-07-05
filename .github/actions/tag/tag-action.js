30eaea71b538409cfc12d845636e2c0979014936
wai-calvin <calvinw@liatrio.com>
2022-07-05 12:17:11 -0700

    checking payload

--- .github/actions/tag/tag-action.js
+++ .github/actions/tag/tag-action.js
@@ -17,48 +17,51 @@ async function action() {
     //Variable for storing the most recent git tag
     const recentGitTag = tags.data[0].name;
 
-    if(releaseCandidate == "true") {
-        if(!recentGitTag.includes("-rc")) {
-            //Grab patch number and increment by 1
-            const oldPatchNum = recentGitTag.split('.').pop();
-            const newPatchNum = Number(oldPatchNum)+1;
-    
-            //Add '-rc' to git tag
-            const createTag = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
-                owner: "wai-calvin",
-                repo: github.context.payload.repository.name,
-                ref: `refs/tags/v1.0.${newPatchNum}-rc0`,
-                sha: github.context.payload.head_commit.id
-            });
-            console.log(`Created tag ${createTag.data.ref} for commit ${createTag.data.object.sha}`);
-        }
-    
-        else {
-            //Grab 
-            const oldRCNum = recentGitTag.match(/\d+$/);
-            const newRCNum = Number(oldRCNum)+1;
-            const gitTagVersion = recentGitTag.split('-', 1);
-    
-            const createTag = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
-                owner: 'wai-calvin',
-                repo: github.context.payload.repository.name,
-                ref: `refs/tags/${gitTagVersion}-rc${newRCNum}`,
-                sha: github.context.payload.head_commit.id
-            });
-            console.log(`Created tag ${createTag.data.ref} for commit ${createTag.data.object.sha}`);
-        }
-    }
+    console.log(github.context);
 
-    else {
-        const gitTagVersion = recentGitTag.split('-', 1);
-        const createTag = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
-            owner: 'wai-calvin',
-            repo: github.context.payload.repository.name,
-            ref: `refs/tags/${gitTagVersion}`,
-            sha: github.context.payload.head_commit.id
-        });
-        console.log(`Created tag ${createTag.data.ref} for commit ${createTag.data.object.sha}`);
-    } 
+    // if(releaseCandidate == "true") {
+    //     if(!recentGitTag.includes("-rc")) {
+    //         //Grab patch number and increment by 1
+    //         const oldPatchNum = recentGitTag.split('.').pop();
+    //         const newPatchNum = Number(oldPatchNum)+1;
+    
+    //         //Add '-rc' to git tag
+    //         const createTag = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+    //             owner: "wai-calvin",
+    //             repo: github.context.payload.repository.name,
+    //             ref: `refs/tags/v1.0.${newPatchNum}-rc0`,
+    //             sha: github.context.payload.head_commit.id
+    //         });
+    //         console.log(`Created tag ${createTag.data.ref} for commit ${createTag.data.object.sha}`);
+    //     }
+    
+    //     else {
+    //         //Grab rc number and increment by 1
+    //         const oldRCNum = recentGitTag.match(/\d+$/);
+    //         const newRCNum = Number(oldRCNum)+1;
+    //         const gitTagVersion = recentGitTag.split('-', 1);
+    
+    //         const createTag = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+    //             owner: 'wai-calvin',
+    //             repo: github.context.payload.repository.name,
+    //             ref: `refs/tags/${gitTagVersion}-rc${newRCNum}`,
+    //             sha: github.context.payload.head_commit.id
+    //         });
+    //         console.log(`Created tag ${createTag.data.ref} for commit ${createTag.data.object.sha}`);
+    //     }
+    // }
+
+    // else {
+    //     //Take out '-rc' from git tag and push tag
+    //     const gitTagVersion = recentGitTag.split('-', 1);
+    //     const createTag = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+    //         owner: 'wai-calvin',
+    //         repo: github.context.payload.repository.name,
+    //         ref: `refs/tags/${gitTagVersion}`,
+    //         sha: github.context.payload.head_commit.id
+    //     });
+    //     console.log(`Created tag ${createTag.data.ref} for commit ${createTag.data.object.sha}`);
+    // } 
 }
 
 action();
